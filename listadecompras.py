c5f74342edcc9c5e1817a5612deed26cd84838c8
Pedro henrique <pedrohenrique754@icloud.com>
2024-09-18 19:51:16 -0300

    mudando lista de compras

--- listadecompras.py
+++ listadecompras.py
@@ -1,1 +1,42 @@
 #!\bin\python3
+
+lista = []
+
+def add_item():
+    print("Informe as informações do item")
+    descricao = input("Descrição:")
+    preco = input("Preço:")
+    item = {
+      "descrição":descricao,
+      "preço":preco
+    }
+    lista.append(item)
+
+def lista_itens():
+    #lista de item com indice|descrição|preço
+    print("Indice|Descrição|Preço")
+    print(indice,"|",descricao,"|",preco)
+    for item in lista:
+        print(item)
+
+def remover_item():
+    pass
+
+def informar_preco():
+    lista_itens()
+    indice = input("Informe o indice:")
+    preco = input("Informe o preço:")
+    lista[int(indice)]["preço"] = preco
+
+def total_da_lista():
+    #retornar total da lista
+    pass
+
+def mostrar_menu():
+    print("0 - Sair")
+
+while True:
+    mostrar_menu()
+    opcao = input("Opção:")
+    if opcao == "0":
+        break
